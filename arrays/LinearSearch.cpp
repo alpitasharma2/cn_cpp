538b8a34465892104973e29bda826297322df9af
Alpita Sharma <69751581+alpitasharma2@users.noreply.github.com>
2021-07-27 16:50:12 +0530

    Update LinearSearch.cpp

--- arrays/LinearSearch.cpp
+++ arrays/LinearSearch.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n,arr[100],searche;
+    cout<<"enter the number of elements: "<<endl;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"enter the element you want to search";
+    cin>>searche;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==searche)
+        {
+            cout<<"element is present at index at: "<<i;
+            break;
+        }
+    }
+    if(i==n)
+    {
+        cout<<"not present";
+    }
+
+    return 0;
+}
 
