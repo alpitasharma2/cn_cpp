53cc7f802610f66e5d5d45b7e2652e4d942be8f9
Alpita Sharma <69751581+alpitasharma2@users.noreply.github.com>
2021-07-30 16:30:09 +0530

    Update bubblesort.cpp

--- arrays/bubblesort.cpp
+++ arrays/bubblesort.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n,arr[100],temp;
+    cout<<"enter the number of elements ";
+    cin>>n;
+    cout<<"enter the elements of an array: ";
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int j=0; j<n; j++)
+    {
+    for(int i=0; i<n-1;i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        cout<<" "<<arr[i];
+    }
+ return 0;
+}
 
